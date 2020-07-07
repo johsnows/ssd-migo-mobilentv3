f0915956b55312d80f2d6685630610e76bbdb983
johsnows <sdut_snow@163.com>
2020-07-07 20:28:12 +0800

    add dail supoort

--- pytorch-ssd-master/vision/ssd/config/mobilenetv1_ssd_config.py
+++ pytorch-ssd-master/vision/ssd/config/mobilenetv1_ssd_config.py
@@ -3,7 +3,7 @@ import numpy as np
 from vision.utils.box_utils import SSDSpec, SSDBoxSizes, generate_ssd_priors
 
 
-image_size = 300
+image_size = 320
 # image_mean = np.array([127, 127, 127])  # RGB layout
 image_mean = np.array([123, 117, 104])  # RGB layout
 # image_std = 128.0
@@ -11,12 +11,12 @@ image_mean = np.array([123, 117, 104])  # RGB layout
 center_variance = 0.1
 size_variance = 0.2
 specs = [
-    SSDSpec(19, 16, SSDBoxSizes(60, 105), [2, 3]),
+    SSDSpec(20, 16, SSDBoxSizes(60, 105), [2, 3]),
     SSDSpec(10, 32, SSDBoxSizes(105, 150), [2, 3]),
     SSDSpec(5, 64, SSDBoxSizes(150, 195), [2, 3]),
-    SSDSpec(3, 100, SSDBoxSizes(195, 240), [2, 3]),
-    SSDSpec(2, 150, SSDBoxSizes(240, 285), [2, 3]),
-    SSDSpec(1, 300, SSDBoxSizes(285, 330), [2, 3])
+    SSDSpec(3, 107, SSDBoxSizes(195, 240), [2, 3]),
+    SSDSpec(2, 160, SSDBoxSizes(240, 285), [2, 3]),
+    SSDSpec(1, 320, SSDBoxSizes(285, 330), [2, 3])
 ]
 
 ##vgg300
@@ -32,14 +32,6 @@ image_std=1.0
 #     SSDSpec(1, 300, SSDBoxSizes(264, 315), [2])
 # ]
 
-specs = [
-    SSDSpec(19, 16, SSDBoxSizes(60, 105), [2, 3]),
-    SSDSpec(10, 32, SSDBoxSizes(105, 150), [2, 3]),
-    SSDSpec(5, 64, SSDBoxSizes(150, 195), [2, 3]),
-    SSDSpec(3, 100, SSDBoxSizes(195, 240), [2, 3]),
-    SSDSpec(2, 150, SSDBoxSizes(240, 285), [2]),
-    SSDSpec(1, 300, SSDBoxSizes(285, 330), [2])
-]
 
 
 
